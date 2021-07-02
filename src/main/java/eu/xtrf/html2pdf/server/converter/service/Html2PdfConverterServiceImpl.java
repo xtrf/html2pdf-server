b6e9e7bd4ea20c6d610cd106117135fb50ac8b89
Kamil Kandefer <kamil.kandefer@xtrf.eu>
2021-07-02 12:26:45 +0200

    ADD CJK fonts and proper encoding

--- src/main/java/eu/xtrf/html2pdf/server/converter/service/Html2PdfConverterServiceImpl.java
+++ src/main/java/eu/xtrf/html2pdf/server/converter/service/Html2PdfConverterServiceImpl.java
@@ -11,31 +11,49 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 @Component
 public class Html2PdfConverterServiceImpl implements Html2PdfConverterService {
     static final Map<String, String> replacementMap = ImmutableMap.<String, String>builder()
-            .put("&nbsp;","&#160;")
-            .put("&lt;","&#60;")
-            .put("&gt;","&#62;")
-            .put("&amp;","&#38;")
-            .put("&quot;","&#34;")
-            .put("&apos;","&#39;")
-            .put("&cent;","&#162;")
-            .put("&pound;","&#163;")
-            .put("&yen;","&#165;")
-            .put("&euro;","&#8364;")
-            .put("&copy;","&#169;")
-            .put("&reg;","&#174;")
+            .put("&nbsp;", "&#160;")
+            .put("&lt;", "&#60;")
+            .put("&gt;", "&#62;")
+            .put("&amp;", "&#38;")
+            .put("&quot;", "&#34;")
+            .put("&apos;", "&#39;")
+            .put("&cent;", "&#162;")
+            .put("&pound;", "&#163;")
+            .put("&yen;", "&#165;")
+            .put("&euro;", "&#8364;")
+            .put("&copy;", "&#169;")
+            .put("&reg;", "&#174;")
             .build();
+
+    private static final Map<String, String> fontFamilies = ImmutableMap.<String, String>builder()
+            .put("Lato", "Lato")
+            .put("Noto_Sans_JP", "Noto Sans JP")
+            .put("Noto_Sans_SC", "Noto Sans SC")
+            .put("Open_Sans", "Open Sans")
+            .put("Poppins", "Poppins")
+            .put("Roboto", "Roboto")
+            .put("Source_Sans_Pro", "Source Sans Pro")
+            .put("Source_Serif_Pro", "Source Serif Pro")
+            .build();
+
+    private ITextRenderer renderer;
+
+    public Html2PdfConverterServiceImpl() throws IOException {
+        renderer = prepareRenderer();
+    }
 
     @Override
     public File generatePdfToFile(String themeContent, String documentContent, String styles, String resourcesPath) throws IOException {
         prepareStylesFile(styles, resourcesPath);
 
         File tempPdfFile = File.createTempFile("generated_", ".pdf");
-
         String pageWithTheme = themeContent.replace("#DOCUMENT_CONTENT", documentContent);
 
         htmlToPdf(pageWithTheme, tempPdfFile, resourcesPath);
@@ -50,7 +68,7 @@ public class Html2PdfConverterServiceImpl implements Html2PdfConverterService {
     }
 
     private static String replaceHtmlEntitiesNamesWithNumbers(String xhtml) {
-        for (Map.Entry<String, String> entry: replacementMap.entrySet()) {
+        for (Map.Entry<String, String> entry : replacementMap.entrySet()) {
             xhtml = xhtml.replace(entry.getKey(), entry.getValue());
         }
         return xhtml;
@@ -62,25 +80,60 @@ public class Html2PdfConverterServiceImpl implements Html2PdfConverterService {
         return replaceHtmlEntitiesNamesWithNumbers(document.html());
     }
 
-    private static ITextRenderer prepareRenderer(String html, String resourcesPath) throws IOException{
-        ITextRenderer renderer = new ITextRenderer();
+    private ITextRenderer prepareRenderer() throws IOException {
+        renderer = new ITextRenderer();
         SharedContext sharedContext = renderer.getSharedContext();
         sharedContext.setPrint(true);
         sharedContext.setInteractive(false);
         sharedContext.setUserAgentCallback(new ConverterOpenPdfUserAgent(renderer.getOutputDevice(), sharedContext));
         sharedContext.getTextRenderer().setSmoothingThreshold(0);
-        // this path to fonts directory works only inside docker, for local execution change to: ./src/main/resources/fonts
-        renderer.getFontResolver().addFontDirectory("/fonts", true); // TODO: move path to configuration file
-        renderer.setDocumentFromString(htmlToXhtml(html), resourcesPath);
-        renderer.layout();
+
+        loadFontsToRenderer(renderer);
 
         return renderer;
     }
 
-    private static void htmlToPdf(String html, File outputPdf, String resourcesPath) throws IOException {
-        ITextRenderer renderer = prepareRenderer(html, resourcesPath);
+    private void htmlToPdf(String html, File outputPdf, String resourcesPath) throws IOException {
+        renderer.setDocumentFromString(htmlToXhtml(html), resourcesPath);
+        renderer.layout();
         try (OutputStream outputStream = new FileOutputStream(outputPdf)) {
             renderer.createPDF(outputStream);
         }
+    }
+
+
+    private static void loadFontsToRenderer(ITextRenderer renderer) throws IOException {
+        // this path to fonts directory works only inside docker, for local execution change to: ./src/main/resources/fonts
+        for (String ttfPath : getTTFFiles("./src/main/resources/fonts")) { // TODO: move path to configuration file
+            String fontFamilyToOverride = findFamilyFont(ttfPath);
+            if (fontFamilyToOverride != null) {
+                renderer.getFontResolver().addFont(ttfPath, fontFamilyToOverride, "Identity-H", true, null);
+            } else {
+                renderer.getFontResolver().addFont(ttfPath, "Identity-H", true);
+            }
+        }
+    }
+
+    private static String findFamilyFont(String ttfPath) {
+        for (Map.Entry<String, String> entry : fontFamilies.entrySet()) {
+            if (ttfPath.contains(entry.getKey())) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
+    private static List<String> getTTFFiles(String dir) {
+        File fontsDir = new File(dir);
+        List<String> ttfFilesPaths = new LinkedList<>();
+
+        for (File file : fontsDir.listFiles()) {
+            if (file.getName().endsWith(".ttf")) {
+                ttfFilesPaths.add(file.getAbsolutePath());
+            } else if (file.isDirectory()) {
+                ttfFilesPaths.addAll(getTTFFiles(file.getAbsolutePath()));
+            }
+        }
+        return ttfFilesPaths;
     }
 }
