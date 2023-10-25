5173df513f076d135fee971f209b3f18705795ad
Wojciech Bubicz <wojciech.bubicz@xtrf.eu>
2023-10-25 16:16:23 +0200

    XDEV-1358 Server-Side Request Forgery (SSRF) in document templates
    (cherry picked from commit 56f051cd72e0c652910c88034b80ac8f3739eae2)

--- backend/xtrf-html2pdf-server/src/main/java/eu/xtrf/html2pdf/server/converter/service/RendererProviderImpl.java
+++ backend/xtrf-html2pdf-server/src/main/java/eu/xtrf/html2pdf/server/converter/service/RendererProviderImpl.java
@@ -17,12 +17,12 @@ public class RendererProviderImpl implements RendererProvider {
     }
 
     @Override
-    public ITextRenderer prepareRenderer(String resourcePath) throws IOException {
+    public ITextRenderer prepareRenderer(String resourcePath, String systemDomain) throws IOException {
         ITextRenderer renderer = new ITextRenderer();
         SharedContext sharedContext = renderer.getSharedContext();
         sharedContext.setPrint(true);
         sharedContext.setInteractive(false);
-        sharedContext.setUserAgentCallback(new ConverterOpenPdfUserAgent(renderer.getOutputDevice(), sharedContext, resourcePath));
+        sharedContext.setUserAgentCallback(new ConverterOpenPdfUserAgent(renderer.getOutputDevice(), sharedContext, resourcePath, systemDomain));
         sharedContext.getTextRenderer().setSmoothingThreshold(0);
 
         fontService.loadFontsToRendererFromResources(renderer, resourcePath);
---
+++ backend/xtrf-html2pdf-server/src/test/java/eu/xtrf/html2pdf/server/converter/service/ConverterOpenPdfUserAgentTest.java
@@ -0,0 +1,61 @@
+package eu.xtrf.html2pdf.server.converter.service;
+
+import eu.xtrf.html2pdf.server.converter.exception.ProcessingFailureException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.ConnectException;
+
+public class ConverterOpenPdfUserAgentTest {
+
+    ConverterOpenPdfUserAgent converterOpenPdfUserAgent = new ConverterOpenPdfUserAgent(null, null, "/res", "xtrf.test.domain");
+
+    @Test(expectedExceptions = ProcessingFailureException.class,
+            expectedExceptionsMessageRegExp = ".* leads to an unauthorized source.")
+    public void should_throw_exception_because_external_resource_url_is_not_allowed() {
+        // given
+        String uri = "https://example.com/file.jpg";
+
+        // when & then
+        converterOpenPdfUserAgent.resolveAndOpenStream(uri);
+    }
+
+    @Test
+    public void should_not_throw_unauthorized_source_exception_because_localhost_resource_url_is_allowed() {
+        // given
+        String uri = "http://localhost/file.jpg";
+
+        // when & then (we do not expect unauthorized source only not existing file)
+        try {
+            converterOpenPdfUserAgent.resolveAndOpenStream(uri);
+        } catch (ProcessingFailureException e) {
+            Assert.assertTrue(e.getCause() instanceof FileNotFoundException || e.getCause() instanceof ConnectException);
+        }
+    }
+
+    @Test(expectedExceptions = ProcessingFailureException.class,
+            expectedExceptionsMessageRegExp = ".* leads to an unauthorized source.")
+    public void should_throw_exception_because_local_file_url_is_not_allowed() {
+        // given
+        String uri = "file:" + this.getClass().getClassLoader().getResource("test_file.txt").getPath();
+
+        // when & then
+        converterOpenPdfUserAgent.resolveAndOpenStream(uri);
+    }
+
+    @Test
+    public void should_not_throw_unauthorized_source_exception_because_system_tmp_file_resource_url_is_allowed() {
+        // given
+        String tmpDir = new File("/tmp").getAbsolutePath();
+        String uri = "file:" + tmpDir + "/html2pdf/f829a2090fea607cdc43827a2fc7af921e9a4aa0/styles.css";
+
+        // when & then (we do not expect unauthorized source only not existing file)
+        try {
+            converterOpenPdfUserAgent.resolveAndOpenStream(uri);
+        } catch (ProcessingFailureException e) {
+            Assert.assertTrue(e.getCause() instanceof FileNotFoundException);
+        }
+    }
+}
