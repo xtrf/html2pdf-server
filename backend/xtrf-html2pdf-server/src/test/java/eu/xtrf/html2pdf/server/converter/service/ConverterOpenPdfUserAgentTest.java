435191a0d8daa475eb8443ea427c40222cab1067
Wojciech Bubicz <wojciech.bubicz@xtrf.eu>
2023-10-27 00:11:12 +0200

    XDEV-1358 Styles.css file uri determined by full path
    (cherry picked from commit df3ed913bc708485e21dd07a8f70696294362aa6)

--- backend/xtrf-html2pdf-server/src/test/java/eu/xtrf/html2pdf/server/converter/service/ConverterOpenPdfUserAgentTest.java
+++ backend/xtrf-html2pdf-server/src/test/java/eu/xtrf/html2pdf/server/converter/service/ConverterOpenPdfUserAgentTest.java
@@ -4,13 +4,14 @@ import eu.xtrf.html2pdf.server.converter.exception.ProcessingFailureException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.ConnectException;
 
 public class ConverterOpenPdfUserAgentTest {
 
-    ConverterOpenPdfUserAgent converterOpenPdfUserAgent = new ConverterOpenPdfUserAgent(null, null, "/res", "xtrf.test.domain", "* {font-family: 'Roboto', 'Open Sans', 'Sans Serif'; font-size: 11px;} header {position: running(header);}");
+    String resourcePath = "/c/system/temp/f829a2090fea6/";
+    ConverterOpenPdfUserAgent converterOpenPdfUserAgent = new ConverterOpenPdfUserAgent(null, null, resourcePath,
+            "xtrf.test.domain", "* {font-family: 'Roboto'; font-size: 11px;}");
 
     @Test(expectedExceptions = ProcessingFailureException.class,
             expectedExceptionsMessageRegExp = ".* leads to an unauthorized source.")
@@ -48,8 +49,7 @@ public class ConverterOpenPdfUserAgentTest {
     @Test
     public void should_not_throw_unauthorized_source_exception_because_system_tmp_file_resource_url_is_allowed() {
         // given
-        String tmpDir = new File("/tmp").getAbsolutePath();
-        String uri = "file:" + tmpDir + "/html2pdf/f829a2090fea607cdc43827a2fc7af921e9a4aa0/styles.css";
+        String uri = "file:" + resourcePath + "styles.css";
 
         // when & then (we do not expect unauthorized source only not existing file)
         try {
