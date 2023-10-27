cd66d672cb62fcda4422d1bce7d6c19c6026728e
Wojciech Bubicz <wojciech.bubicz@xtrf.eu>
2023-10-27 16:33:09 +0200

    XDEV-1358 Fixes after review
    (cherry picked from commit 4ed6fa3769a7130f70524b21c88799a69ec17f55)

--- backend/xtrf-html2pdf-server/src/test/java/eu/xtrf/html2pdf/server/converter/service/ConverterOpenPdfUserAgentTest.java
+++ backend/xtrf-html2pdf-server/src/test/java/eu/xtrf/html2pdf/server/converter/service/ConverterOpenPdfUserAgentTest.java
@@ -4,23 +4,33 @@ import eu.xtrf.html2pdf.server.converter.exception.ProcessingFailureException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ConnectException;
+import java.util.stream.Collectors;
+
+import static eu.xtrf.test.assertions.ExceptionAssertions.assertException;
+import static eu.xtrf.test.assertions.ExceptionAssertions.catchException;
 
 public class ConverterOpenPdfUserAgentTest {
 
     String resourcePath = "/c/system\\temp/f829a2090fea6/";
-    ConverterOpenPdfUserAgent converterOpenPdfUserAgent = new ConverterOpenPdfUserAgent(null, null, resourcePath,
-            "xtrf.test.domain", "* {font-family: 'Roboto'; font-size: 11px;}");
+    String sampleStyleCss = "* {font-family: 'Roboto'; font-size: 11px;}";
+    ConverterOpenPdfUserAgent converterOpenPdfUserAgent = new ConverterOpenPdfUserAgent(null, null, resourcePath, "xtrf.test.domain", sampleStyleCss);
 
-    @Test(expectedExceptions = ProcessingFailureException.class,
-            expectedExceptionsMessageRegExp = ".* leads to an unauthorized source.")
+    @Test
     public void should_throw_exception_because_external_resource_url_is_not_allowed() {
         // given
         String uri = "https://example.com/file.jpg";
 
-        // when & then
-        converterOpenPdfUserAgent.resolveAndOpenStream(uri);
+        // when
+        Exception exception = catchException(() -> converterOpenPdfUserAgent.resolveAndOpenStream(uri));
+
+        // then
+        assertException(ProcessingFailureException.class, exception);
+        Assert.assertTrue(exception.getMessage().matches(".* leads to an unauthorized source."));
     }
 
     @Test
@@ -28,22 +38,24 @@ public class ConverterOpenPdfUserAgentTest {
         // given
         String uri = "http://localhost/file.jpg";
 
-        // when & then (we do not expect unauthorized source only not existing file)
-        try {
-            converterOpenPdfUserAgent.resolveAndOpenStream(uri);
-        } catch (ProcessingFailureException e) {
-            Assert.assertTrue(e.getCause() instanceof FileNotFoundException || e.getCause() instanceof ConnectException);
-        }
+        // when
+        Exception exception = catchException(() -> converterOpenPdfUserAgent.resolveAndOpenStream(uri));
+
+        // then (we do not expect unauthorized source only not existing file)
+        Assert.assertTrue(exception.getCause() instanceof FileNotFoundException || exception.getCause() instanceof ConnectException);
     }
 
-    @Test(expectedExceptions = ProcessingFailureException.class,
-            expectedExceptionsMessageRegExp = ".* leads to an unauthorized source.")
+    @Test
     public void should_throw_exception_because_local_file_url_is_not_allowed() {
         // given
         String uri = "file:" + this.getClass().getClassLoader().getResource("test_file.txt").getPath();
 
-        // when & then
-        converterOpenPdfUserAgent.resolveAndOpenStream(uri);
+        // when
+        Exception exception = catchException(() -> converterOpenPdfUserAgent.resolveAndOpenStream(uri));
+
+        // then
+        assertException(ProcessingFailureException.class, exception);
+        Assert.assertTrue(exception.getMessage().matches(".* leads to an unauthorized source."));
     }
 
     @Test
@@ -51,11 +63,11 @@ public class ConverterOpenPdfUserAgentTest {
         // given
         String uri = "file:/" + resourcePath + "styles.css";
 
-        // when & then (we do not expect unauthorized source only not existing file)
-        try {
-            converterOpenPdfUserAgent.resolveAndOpenStream(uri);
-        } catch (ProcessingFailureException e) {
-            Assert.assertTrue(e.getCause() instanceof FileNotFoundException);
-        }
+        // when
+        InputStream inputStream = converterOpenPdfUserAgent.resolveAndOpenStream(uri);
+
+        // then
+        String result = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
+        Assert.assertEquals(result, sampleStyleCss);
     }
 }
